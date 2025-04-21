e9a56a77e29356cc19e16e6a57e555b74c53b7bf
dhanajit96 <ajitdhanje@gmail.com>
2025-04-21 21:31:43 +0530

    added delimiter and console input in string_calculator

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,18 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    numbers.split(/,|\\n/).map(&:to_i).sum
+  end
+end
+
+# console application
+if __FILE__ == $0
+  puts 'Enter numbers with delimiter : '
+  input = gets.chomp
+  begin
+    result = StringCalculator.add(input)
+    puts "Result: #{result}"
+  rescue StandardError => e
+    puts "Error: #{e.message}"
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,18 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add('')).to eq(0)
       end
     end
+
+    context 'If input is single number string' do
+      it 'Sum will returns same number' do
+        expect(StringCalculator.add('5')).to eq(5)
+      end
+    end
+
+    context 'input is two digits number separted by delimiter (comma to \n)' do
+      it 'returns Sum of two number is provided' do
+        expect(StringCalculator.add('5,5')).to eq(10)
+        expect(StringCalculator.add('5\n15')).to eq(20)
+      end
+    end
   end
 end
